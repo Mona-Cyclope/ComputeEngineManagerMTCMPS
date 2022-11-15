507e4e3425dca66f7b537297b387782b9d7f5165
Mario Viti <mario.viti@cyclope.ai>
2022-11-15 16:06:56 +0100

    [UPD] refactoring code and adding log

--- compute_engine_manager.py
+++ compute_engine_manager.py
@@ -11,7 +11,34 @@ from threading import Thread
 import numpy as np
 
 
+class ComputeEngine(metaclass=ABCMeta):
+    """Abstract class for the compute engine"""
+    
+    @abstractmethod
+    def load(self): pass
+    
+    @abstractmethod
+    def process(self, data:list): pass
+    
+class CommQueue(metaclass=ABCMeta):
+    """Abstract class for the communication queue. A communication queue is used between a client and a compute engine (server)"""
+
+    @abstractmethod
+    def __init__(self, id): pass
+    
+    @abstractmethod
+    def get(self): pass
+    
+    @abstractmethod
+    def put(self, request_id, reques_data): pass
+    
+    @abstractmethod
+    def empty(self): pass
+
+
 class ComputeEngineClient():
+    
+    """compute engine client communicating with compute engines"""
     
     def __init__(self, server_queues, client_queue):
         self._LOG = logging.getLogger(self.__class__.__name__)
@@ -30,20 +57,99 @@ class ComputeEngineClient():
         client_id,out_data = self.client_queue.get()
         return out_data
     
-class CommQueue(metaclass=ABCMeta):
+class ComputeEngineManager():
     
-    @abstractmethod
-    def __init__(self, id): pass
+    """compute engine manager puts in communication compute engines(servers) and clients via communication queues"""
     
-    @abstractmethod
-    def get(self): pass
+    def __init__(self, compute_engines, num_clients=1, batch_size=1, comm_queue_class=CommQueue, session_prefix=None):
+        
+        self._LOG = logging.getLogger(self.__class__.__name__)
+        self.num_compute_engines = len(compute_engines)
+        self.num_clients = num_clients
+        if session_prefix is None: 
+            session_prefix = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+            self._LOG.info("session prefix not defined one will be created: {}".format(session_prefix))
+        self.session_prefix = session_prefix
+        self._LOG.info("initializeing identifiers for clients and server")
+        self.initialize_ids()
+        self.batch_size = batch_size
+        self.comm_queue_class = comm_queue_class
+        self.compute_engines = compute_engines
+        self._LOG.info("initialize_servers")
+        self.initialize_servers()
+        self._LOG.info("initialize_clients")
+        self.initialize_clients()
     
-    @abstractmethod
-    def put(self, request_id, reques_data): pass
+    def initialize_servers(self):
+        compute_engines = self.compute_engines
+        batch_size = self.batch_size
+        comm_queue_class = self.comm_queue_class
+        server_queues, server_processes = [], []
+        client_ids = self.client_ids
+        server_ids = self.server_ids
+        # create the queues to be passed to each compute engine
+        # each compute engine is assigned a Process in wich the inference_manager method will run
+        # each compute engins is also assigned a comunitacion queue to listen to
+        client_queues = { client_id: comm_queue_class(client_id) for client_id in client_ids }
+        for ce_idx, compute_engine in enumerate(compute_engines):
+            server_queue = comm_queue_class( server_ids[ce_idx] )
+            server_process = mtp.Process(target=self.inference_manager, args=[ compute_engine, server_queue, client_queues, batch_size ]) 
+            server_process.daemon = True  
+            server_queues.append(server_queue)
+            server_processes.append(server_process)
+        self.client_queues = client_queues
+        self.server_queues = server_queues
+        self.server_processes = server_processes
+            
+    def initialize_clients(self):
+        client_ids = self.client_ids
+        client_queues = self.client_queues
+        server_queues = self.server_queues
+        clients = [ ComputeEngineClient( server_queues, client_queues[client_id]) for client_id in client_ids ]
+        self.clients = clients
+        
+    def initialize_ids(self):
+        self.client_ids = [ "{}_client_{}".format(self.session_prefix, i) for i in range(self.num_clients) ]
+        self.server_ids = [ "{}_server_{}".format(self.session_prefix, i) for i in range(self.num_compute_engines) ]
+            
+    def start(self):
+        self._LOG.info("starting servers")
+        for server_process in self.server_processes: server_process.start()
+            
+    def get_clients(self): return self.clients        
+        
+    @staticmethod
+    def inference_manager(compute_engine, server_queue, client_queues, batch_size):
+        """main method for the compute engine, an infinite loop that listes to data in the communication queue and replies to clients
+        Args:
+            compute_engine (ComputeEngine): the main compute engine implementing the load and process method
+            server_queue (CommQueue): the queue onto wich clients send their requests
+            client_queues (CommQueue dict): dictionary containing communication queues to reply to clients
+            batch_size (int): batching request from different clients
+        """
+        compute_engine.load()
+        while True:
+            client_ids, in_datas = [], []
+            for i in range(batch_size):
+                if server_queue.empty(): break
+                client_id, in_data = server_queue.get()
+                client_ids.append(client_id)
+                in_datas.append(in_data)
+                
+            if len(in_datas)>0:
+                out_datas = compute_engine.process(in_datas)
+                for client_id, out_data in zip(client_ids, out_datas):
+                    client_queues[client_id].put(client_id,out_data)
     
-    @abstractmethod
-    def empty(self): pass
+# TEST IMPLEMENTATION
     
+class TestCE(ComputeEngine):
+    
+    def load(self): pass
+    
+    def process(self, data): 
+        time.sleep(0.01)
+        return data
     
 class MTPCommQueue(CommQueue):
     
@@ -60,83 +166,25 @@ class MTPCommQueue(CommQueue):
     def empty(self):
         return self.q.empty()
     
-class ComputeEngineManager():
-    
-    def __init__(self, compute_engines, num_clients=1, batch_size=1, comm_queue_class=CommQueue, session_prefix=None):
-        self._LOG = logging.getLogger(self.__class__.__name__)
-        server_ids, server_queues, server_processes = [], [], []
-        if session_prefix is None: session_prefix = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
-        client_ids = [ "{}_client_{}".format(session_prefix, i) for i in range(num_clients) ]
-        client_queues = { client_id: comm_queue_class(client_id) for client_id in client_ids }
-        for ce_idx, compute_engine in enumerate(compute_engines):
-            server_ids += [ "{}_server_{}".format(session_prefix, ce_idx) ]
-            server_queue = comm_queue_class( server_ids[-1] )
-            server_process = mtp.Process(target=self.inference_manager, args=[ compute_engine, server_queue, client_queues, batch_size ]) 
-            server_process.daemon = True  
-            server_queues.append(server_queue)
-            server_processes.append(server_process)
-        clients = [ ComputeEngineClient( server_queues, client_queues[client_id]) for client_id in client_ids ]
-        self.clients = clients
-        self.server_processes = server_processes
-        self.batch_size = batch_size
-        self.server_ids = server_ids
-        self.client_ids = client_ids
-        self._LOG.info("instantiation of servers and clients")
-            
-    def start(self):
-        self._LOG.info("startimg servers")
-        for server_process in self.server_processes: server_process.start()
-            
-    def get_clients(self): return self.clients        
-        
-    @staticmethod
-    def inference_manager(compute_engine, server_queue, client_queues, batch_size):
-        compute_engine.load()
-        while True:
-            client_ids, in_datas = [], []
-            for i in range(batch_size):
-                if server_queue.empty(): break
-                client_id, in_data = server_queue.get()
-                client_ids.append(client_id)
-                in_datas.append(in_data)
-                
-            if len(in_datas)>0:
-                out_datas = compute_engine.process(in_datas)
-                for client_id, out_data in zip(client_ids, out_datas):
-                    client_queues[client_id].put(client_id,out_data)
-            
-class ComputeEngine(metaclass=ABCMeta):
-    
-    @abstractmethod
-    def load(self): pass
-    
-    @abstractmethod
-    def process(self, data:list): pass
-    
-class TestCE(ComputeEngine):
-    
-    def load(self): pass
-    
-    def process(self, data): 
-        time.sleep(0.01)
-        return data
-    
 def test_client(client):
-    for inp in range(100):
+    for inp in range(3):
         out = client.request(inp)
         print("input: {}, output: {}, =============> success: {} ============> client_id: {}".format(inp, out, inp==out, client.client_id)) 
     
 if __name__ == "__main__":
+
+    logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',level=logging.INFO)
     
     list_of_ce = [ TestCE(), TestCE(), TestCE() ]
-    compute_engine = ComputeEngineManager(list_of_ce, num_clients=3, batch_size=1, comm_queue_class=MTPCommQueue)
+    compute_engine = ComputeEngineManager(list_of_ce, num_clients=10, batch_size=1, comm_queue_class=MTPCommQueue)
     compute_engine.start()
     clients = compute_engine.get_clients()
     
     t0 = time.time()
     print("============= SINGLE PROCESS =============")    
     # sequential
-    for inp in range(100):
+    for inp in range(3):
         out = clients[inp%len(clients)].request(inp)
         print("input: {}, output: {}, =============> success: {}".format(inp, out, inp==out)) 
     tT = time.time()
